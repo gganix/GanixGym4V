0c4593247bf66bdd52d313167b4daac445079c3c
gganix <ggorosairi1@educacion.navarra.es>
2024-12-08 23:06:23 +0100

    componentes y servicio

---
+++ src/app/components/lista-actividades/lista-actividades.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-actividades',
+  imports: [],
+  templateUrl: './lista-actividades.component.html',
+  styleUrl: './lista-actividades.component.scss'
+})
+export class ListaActividadesComponent {
+
+}
